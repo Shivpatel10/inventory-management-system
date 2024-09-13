f1189fbcd5a567c6e16b69dea06e47b8aa3f56e3
Shiv Patel <shivpatel0204@gmail.com>
2024-09-13 15:48:31 -0400

    Initialize inventory with product objects

---
+++ inventory_management_system.js
@@ -0,0 +1,9 @@
+// Task 1: Create an Inventory Array of Product Objects
+let inventory = [
+{ name:"mouse",    price: 10, quantity: 100, lowStockLevel: 5},
+{ name:"charger",  price: 20, quantity: 100, lowStockLevel: 5},
+{ name:"cpu",      price: 30, quantity: 100, lowStockLevel: 5},
+{ name:"gpu",      price: 40, quantity: 100, lowStockLevel: 5},
+{ name:"Keyboard", price: 50, quantity: 100, lowStockLevel: 5},
+];
+
