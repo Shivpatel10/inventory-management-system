b0a60ad2bc8b4ec12f9eeaf227321a5cb459e356
Shiv Patel <shivpatel0204@gmail.com>
2024-09-14 22:50:43 -0400

    Create displayProductDetails function

--- inventory_management_system.js
+++ inventory_management_system.js
@@ -6,21 +6,19 @@ let inventory = [
 { name:"charger",  price: 20, quantity: 42, lowStockLevel: 5},
 { name:"cpu",      price: 30, quantity: 36, lowStockLevel: 5},
 { name:"gpu",      price: 40, quantity: 32, lowStockLevel: 5},
-{ name:"Keyboard", price: 50, quantity: 96, lowStockLevel: 5},
+{ name:"Keyboard", price: 50, quantity: 96, lowStockLevel: 5}
 ];
 
 
 // Task 2: Create a Function to Display Product Details
    
 function displayProductDetails(inventory) {
-const product = inventory.find(inventory => inventory.name, inventory.price, inventory.quantity ); //used the find function to recall these elements from inventory array
-let stockStatus = product.quantity >= product.lowStockLevel ? "In Stock" : "Low Stock"; //function that will check if the products stockStatus is In Stock or LowStock
-
-    console.log(`Product Name: ${product.name}`);
-    console.log(`Product Price: ${product.price}`);
-    console.log(`Quantity of Stock: ${product.quantity}`);
-    console.log(`Products Stock Status: ${stockStatus}`);
-};
-console.log(displayProductDetails(inventory)); // console log that will output the final result.
-
-
+//const product = inventory.quantity <= inventory.lowStockLevel;  // used the find function to recall these elements from inventory array
+//let stockStatus = product.quantity >= product.lowStockLevel ? "In Stock" : "Low Stock"; //function that will check if the products stockStatus is In Stock or LowStock
+const stockStatus = inventory.quantity <= inventory.lowStockLevel ? "Low Stock" : "In Stock"
+    return `${inventory.name} is ${stockStatus}`
+}
+    console.log(`Product Name: ${inventory[0].name}`);
+    console.log(`Product Price: ${inventory[0].price}`);
+    console.log(`Quantity of Stock: ${(inventory[0].quantity)}`);
+    console.log(displayProductDetails(inventory[0])); // console log that will output the final result.
